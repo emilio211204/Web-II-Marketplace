dd28b28cc1db6262d6e7398c0488b17b379633b7
heydi20 <heydipantoja1@gmail.com>
2025-03-01 22:49:48 -0500

    cambios en navegacion enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'nosotros',
+        component:NosotrosComponent
+    },
+    {
+        path:'contacto',
+        component:ContactoComponent
+    },
+    {
+        path:'servicios',
+        component:ContactoComponent
+    },
+    {
+        path:'formulario',
+        component:FormularioComponent
+    },
+    {
+        path:'',
+        redirectTo:'/home',
+        pathMatch:'full'
+    },
+];
