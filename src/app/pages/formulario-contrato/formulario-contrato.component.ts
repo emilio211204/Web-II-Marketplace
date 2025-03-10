5028c04998eaa9dcde2e5562fda6ffde18e483ce
Mc-Cristian <sangoluisacristian75@gmail.com>
2025-03-10 01:18:32 -0500

    home

---
+++ src/app/pages/formulario-contrato/formulario-contrato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-contrato',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-contrato.component.html',
+  styleUrl: './formulario-contrato.component.css'
+})
+export class FormularioContratoComponent {
+
+}
