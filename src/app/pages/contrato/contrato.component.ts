eda8507d005790dacbc6b53448d3310262d900e8
Mc-Cristian <sangoluisacristian75@gmail.com>
2025-03-13 07:49:19 -0500

    diseño51

--- src/app/pages/contrato/contrato.component.ts
+++ src/app/pages/contrato/contrato.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormularioCComponent } from "../../components/formulario-c/formulario-c.component";
+import { FormularioContratoComponent } from "../formulario-contrato/formulario-contrato.component";
+import { FormularioCronComponent } from "../../components/formulario-cron/formulario-cron.component";
 
 @Component({
   selector: 'app-contrato',
   standalone: true,
-  imports: [FormularioCComponent],
+  imports: [FormularioCComponent, FormularioContratoComponent, FormularioCronComponent],
   templateUrl: './contrato.component.html',
   styleUrl: './contrato.component.css'
 })
--- src/app/service/contrato.service.ts
+++ src/app/service/contrato.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cliente } from './cliente.service';
+import { Prestaciones } from './prestaciones.service';
 
 
 export interface Contrato {
